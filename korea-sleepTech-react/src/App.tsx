7a1b387d046433846c38c994c0a012d4ca7be6c3
chang0506 <j8970@naver.com>
2025-04-07 16:24:16 +0900

    0407 - React Zustand

--- korea-sleepTech-react/src/App.tsx
+++ korea-sleepTech-react/src/App.tsx
@@ -11,6 +11,9 @@ import Hooks from '@/pages/b_hooks';
 import Router from '@/pages/c_router';
 import NaviBar from './components/NaviBar';
 import RouterPractice from '@/pages/d_router_practice'
+import Review from '@/pages/e_review'
+import Http from '@/pages/f_http'
+import Global from '@/pages/g_global_state'
 
 //& 기본 Vite React 앱의 경로
 // : http://localhost:5173
@@ -38,6 +41,10 @@ function App() {
         */}
         <Route path='/router/*' element={<Router />} />
         <Route path='/router-practice/*' element={<RouterPractice />} />
+        <Route path='/review/*' element={<Review />} />
+        <Route path='/http' element={<Http />} />
+        <Route path='/global-state' element={<Global />} />
+        
       </Routes>
     </>
   )
---
+++ korea-sleepTech-react/src/pages/e_review/pages/Home.tsx
@@ -0,0 +1,23 @@
+import React from 'react'
+import { useNavigate } from 'react-router-dom'
+
+function Home() {
+  const navigate = useNavigate();
+
+  const gotoAbout = () => {
+    navigate('/review/about', {
+      state: {
+              from: '홈페이지',
+      message: '환영합니다! 이것은 홈페이지로 부터 전달된 메시지 입니다.'
+      }
+    });
+  }
+  return (
+    <div>
+      <h3>홈 페이지</h3>
+      <button onClick={gotoAbout}>소개 페이지로 이동</button>
+    </div>
+  )
+}
+
+export default Home
